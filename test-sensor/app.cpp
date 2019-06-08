f071c0e24988c54884c28130ba1b65f3eef1a1aa
Wataru Taniguchi <wataru@taniguchifamily.com>
2019-06-08 10:08:00 +0900

    application "test-sensor" added for playing with color sensor in RGB/HSV mode

---
+++ test-sensor/app.cpp
@@ -0,0 +1,151 @@
+#include "ev3api.h"
+#include "app.h"
+#include "TouchSensor.h"
+#include "SonarSensor.h"
+#include "ColorSensor.h"
+#include "GyroSensor.h"
+#include "Motor.h"
+#include "Clock.h"
+
+using namespace ev3api;
+
+#define DEBUG
+
+#ifdef DEBUG
+#define _debug(x) (x)
+#else
+#define _debug(x)
+#endif
+
+/* Bluetooth */
+static int32_t   bt_cmd = 0;      /* Bluetoothコマンド 1:リモートスタート */
+static FILE     *bt = NULL;      /* Bluetoothファイルハンドル */
+
+/* 下記のマクロは個体/環境に合わせて変更する必要があります */
+#define GYRO_OFFSET           0  /* ジャイロセンサオフセット値(角速度0[deg/sec]時) */
+#define LIGHT_WHITE          55  /* 白色の光センサ値 */
+#define LIGHT_BLACK           0  /* 黒色の光センサ値 */
+#define SONAR_ALERT_DISTANCE 30  /* 超音波センサによる障害物検知距離[cm] */
+//#define DEVICE_NAME     "ET0"  /* Bluetooth名 hrp2/target/ev3.h BLUETOOTH_LOCAL_NAMEで設定 */
+//#define PASS_KEY        "1234" /* パスキー    hrp2/target/ev3.h BLUETOOTH_PIN_CODEで設定 */
+#define CMD_START         '1'    /* リモートスタートコマンド */
+
+/* LCDフォントサイズ */
+#define CALIB_FONT (EV3_FONT_SMALL)
+#define CALIB_FONT_WIDTH (6/*TODO: magic number*/)
+#define CALIB_FONT_HEIGHT (8/*TODO: magic number*/)
+
+/* オブジェクトへのポインタ定義 */
+TouchSensor*    touchSensor;
+SonarSensor*    sonarSensor;
+ColorSensor*    colorSensor;
+GyroSensor*     gyroSensor;
+Clock*          clock;
+
+typedef struct {
+    uint16_t h; // Hue
+    uint16_t s; // Saturation
+    uint16_t v; // Value of brightness
+} hsv_raw_t;
+
+void rgb_to_hsv(rgb_raw_t rgb, hsv_raw_t& hsv) {
+    uint16_t max, min, cr, cg, cb, h;
+
+    max = rgb.r;
+    if(max < rgb.g) max = rgb.g;
+    if(max < rgb.b) max = rgb.b;
+    
+    min = rgb.r;
+    if(min > rgb.g) min = rgb.g;
+    if(min > rgb.b) min = rgb.b;
+
+    hsv.v = max;
+    
+    if (!max) {
+        hsv.s = 0;
+        hsv.h = 0;
+    } else {
+        hsv.s = 255 * (max - min) / (double)max;
+        cr = (max - rgb.r) / (double)(max - min);
+        cg = (max - rgb.g) / (double)(max - min);
+        cb = (max - rgb.b) / (double)(max - min);
+        
+        if (max == rgb.r) {
+            h = cb - cg;
+        } else if (max == rgb.g) {
+            h = 2 + cr - cb;
+        } else {
+            h = 4 + cg - cr;
+        }
+        h *= 60;
+        if (h < 0) h += 360;
+        hsv.h = h;
+    }
+}
+
+void main_task(intptr_t unused)
+{
+    rgb_raw_t cur_rgb;
+    hsv_raw_t cur_hsv;
+
+    // display identity on LCD
+    ev3_lcd_fill_rect(0, 0, EV3_LCD_WIDTH, EV3_LCD_HEIGHT, EV3_LCD_WHITE);
+    ev3_lcd_draw_string("test-sensor", 0, CALIB_FONT_HEIGHT*1);
+
+    /*
+    // open Bluetooth file and activate the task
+    bt = ev3_serial_open_file(EV3_SERIAL_BT);
+    assert(bt != NULL);
+    act_tsk(BT_TASK);
+    */
+
+    //touchSensor = new TouchSensor(PORT_1);
+    //sonarSensor = new SonarSensor(PORT_2);
+    colorSensor = new ColorSensor(PORT_3);
+    //gyroSensor  = new GyroSensor(PORT_4);
+    clock       = new Clock();
+
+    //gyroSensor->reset();
+    ev3_led_set_color(LED_GREEN); // indicate that it is ready
+
+    while(1)
+    {
+        if (ev3_button_is_pressed(BACK_BUTTON)) break;
+
+        colorSensor->getRawColor(cur_rgb);
+        rgb_to_hsv(cur_rgb, cur_hsv);
+        syslog(LOG_NOTICE, "%06lu: r = %03d, g = %03d, b = %03d", clock->now(), cur_rgb.r, cur_rgb.g, cur_rgb.b);
+        syslog(LOG_NOTICE, "%06lu: h = %03d, s = %03d, v = %03d", clock->now(), cur_hsv.h, cur_hsv.s, cur_hsv.v);
+        clock->sleep(500); /* execute in every 500 msec */
+    }
+    //ter_tsk(BT_TASK);
+    //fclose(bt);
+
+    ext_tsk();
+}
+
+
+
+//*****************************************************************************
+// 関数名 : bt_task
+// 引数 : unused
+// 返り値 : なし
+// 概要 : Bluetooth通信によるリモートスタート。 Tera Termなどのターミナルソフトから、
+//       ASCIIコードで1を送信すると、リモートスタートする。
+//*****************************************************************************
+void bt_task(intptr_t unused)
+{
+    while(1)
+    {
+        uint8_t c = fgetc(bt); /* 受信 */
+        switch(c)
+        {
+        case '1':
+            bt_cmd = 1;
+            break;
+        default:
+            break;
+        }
+        fputc(c, bt); /* エコーバック */
+    }
+}
