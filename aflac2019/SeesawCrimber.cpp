d73a46a6e573e049e1436dd2ac1e8c088c782b16
RYO MINAGAWA <51693340+RYOMINAGAWA@users.noreply.github.com>
2019-08-08 15:17:21 +0900

    実機テストのためのコードを追加。

--- aflac2019/SeesawCrimber.cpp
+++ aflac2019/SeesawCrimber.cpp
@@ -11,34 +11,175 @@
 
 SeesawCrimber::SeesawCrimber(Motor* lm, Motor* rm, Motor* tm, GyroSensor* gs, ColorSensor* cs) : LineTracer(lm, rm, tm, gs, cs) {
     _debug(syslog(LOG_NOTICE, "%08lu, SeesawCrimber constructor", clock->now()));
-    startTime=0;
-    counter=0;
-    speed=0;
 }
 
 void SeesawCrimber::haveControl() {
     activeNavigator = this;
+
+    // private変数の初期化
+    s_time = 0;
+    s_counter = 0;
+    s_speed = 0;
+    s_mode = SEESAW_00;
+    s_angle = TAIL_ANGLE_NORMAL_RUN;
+
+    // ログ出力
     syslog(LOG_NOTICE, "%08lu, SeesawCrimber has control", clock->now());
 }
 
 void SeesawCrimber::operate() {
-	_debug(syslog(LOG_NOTICE,"%08lu, SeesawCrimber operate start", clock->now()));
+
+	// 初期状態での尻尾角度をTAIL_ANGLE_NORMAL_RUNに合わせて設定。
 	controlTail(TAIL_ANGLE_NORMAL_RUN);
 
-	forward = START_SPEED;
-	turn = 0;
+	switch( s_mode ){
+	case SEESAW_00:
+		if ( ++s_counter > PERIOD_SEESAW ){
+			s_mode = SEESAW_01;
+			s_counter = 0;
+		}
+		pwm_L = 0;
+		pwm_R = 0;
+		s_angle = 80;
+		break;
+	case SEESAW_01:
+		if ( ++s_counter > PERIOD_SEESAW ){
+			s_mode = SEESAW_02;
+			s_counter = 0;
+		}
+		pwm_L = 50;
+		pwm_R = 50;
+		s_angle = 80;
+		break;
+	case SEESAW_02:
+		if ( ++s_counter > PERIOD_SEESAW ){
+			s_mode = SEESAW_03;
+			s_counter = 0;
+		}
+		pwm_L = 15;
+		pwm_R = 15;
+		s_angle = 50;
+		break;
 
-	pwm_L = forward;
-	pwm_R = forward;
+	case SEESAW_03:
+		if ( ++s_counter > PERIOD_SEESAW ){
+			s_mode = SEESAW_04;
+			s_counter = 0;
+		}
+		pwm_L = 0;
+		pwm_R = 0;
+		s_angle = 40;
+		break;
 
+	case SEESAW_04:
+		if ( ++s_counter > PERIOD_SEESAW ){
+			s_mode = SEESAW_05;
+			s_counter = 0;
+		}
+		pwm_L = -15;
+		pwm_R = -15;
+		s_angle = 40;
+		break;
+
+	case SEESAW_05:
+		if ( ++s_counter > PERIOD_SEESAW ){
+			s_mode = SEESAW_06;
+			s_counter = 0;
+		}
+		pwm_L = -15;
+		pwm_R = -15;
+		s_angle = 50;
+		break;
+
+	case SEESAW_06:
+		if ( ++s_counter > PERIOD_SEESAW ){
+			s_mode = SEESAW_07;
+			s_counter = 0;
+		}
+		pwm_L = 0;
+		pwm_R = 0;
+		s_angle = 50;
+		break;
+
+	case SEESAW_07:
+		if ( ++s_counter > PERIOD_SEESAW ){
+			s_mode = SEESAW_08;
+			s_counter = 0;
+		}
+		pwm_L = 15;
+		pwm_R = 15;
+		s_angle = 50;
+		break;
+
+	case SEESAW_08:
+		if ( ++s_counter > PERIOD_SEESAW ){
+			s_mode = SEESAW_09;
+			s_counter = 0;
+		}
+		pwm_L = 15;
+		pwm_R = 15;
+		s_angle = 40;
+		break;
+
+	case SEESAW_09:
+		if ( ++s_counter > PERIOD_SEESAW ){
+			s_mode = SEESAW_10;
+			s_counter = 0;
+		}
+		pwm_L = 0;
+		pwm_R = 0;
+		s_angle = 40;
+		break;
+
+	case SEESAW_10:
+		if ( ++s_counter > PERIOD_SEESAW ){
+			s_mode = SEESAW_11;
+			s_counter = 0;
+		}
+		pwm_L = 15;
+		pwm_R = 15;
+		s_angle = 40;
+		break;
+
+	case SEESAW_11:
+		if ( ++s_counter > PERIOD_SEESAW ){
+			s_mode = SEESAW_12;
+			s_counter = 0;
+		}
+		pwm_L = 0;
+		pwm_R = 0;
+		s_angle = 60;
+		break;
+
+	case SEESAW_12:
+		if ( ++s_counter > PERIOD_SEESAW ){
+			s_mode = SEESAW_01;
+			s_counter = 0;
+		}
+		pwm_L = 0;
+		pwm_R = 0;
+		s_angle = 80;
+		break;
+
+	case SEESAW_13:
+		pwm_L = 15;
+		pwm_R = 15;
+		s_angle = 80;
+		break;
+
+	}
+
+	// 左右モーターと尻尾モーターへ値を渡す
 	leftMotor->setPWM(pwm_L);
 	rightMotor->setPWM(pwm_R);
+	controlTail(s_angle);
 
-	// display pwm in every PERIOD_TRACE_MSG ms */
-	    if (++trace_pwmLR * PERIOD_NAV_TSK >= PERIOD_TRACE_MSG) {
-	        trace_pwmLR = 0;
-	        _debug(syslog(LOG_NOTICE, "%08u, LimboDancer::operate(): pwm_L = %d, pwm_R = %d", clock->now(), pwm_L, pwm_R));
-	    }
+	// ログを PERIOD_TRAVE_MSG ms で出力する
+	if (++s_trace_counter * PERIOD_NAV_TSK >= PERIOD_TRACE_MSG ) {
+		s_trace_counter = 0;
+		_debug(syslog(LOG_NOTICE, "%08u, SeesawCrimber::operate(): case_no = %d, tail_angle = %d", clock->now(), s_mode, s_angle));
+		_debug(syslog(LOG_NOTICE, "%08u, SeesawCrimber::operate(): pwm_L = %d, pwm_R = %d", clock->now(), pwm_L, pwm_R));
+	}
 }
 
 SeesawCrimber::~SeesawCrimber() {
--- aflac2019/SeesawCrimber.hpp
+++ aflac2019/SeesawCrimber.hpp
@@ -13,13 +13,34 @@
 
 #define TAIL_ANGLE_NORMAL_RUN    80  /* 通常走行時のしっぽの角度[度] */
 #define TAIL_ANGLE_SEESAW_FIRST  40  /* シーソー上り始めのしっぽの角度[度] */
+
+#define PERIOD_SEESAW	250		// 次の状態へ遷移するまでの回数
+
+#define SEESAW_00	0	// 状態遷移番号
+#define SEESAW_01	1	// 状態遷移番号
+#define SEESAW_02	2	// 状態遷移番号
+#define SEESAW_03	3	// 状態遷移番号
+#define SEESAW_04	4	// 状態遷移番号
+#define SEESAW_05	5	// 状態遷移番号
+#define SEESAW_06	6	// 状態遷移番号
+#define SEESAW_07	7	// 状態遷移番号
+#define SEESAW_08	8	// 状態遷移番号
+#define SEESAW_09	9	// 状態遷移番号
+#define SEESAW_10	10	// 状態遷移番号
+#define SEESAW_11	11	// 状態遷移番号
+#define SEESAW_12	12	// 状態遷移番号
+#define SEESAW_13	13	// 状態遷移番号
+
 #define START_SPEED 0  /* START時の各モーターに与える値 */
 
 class SeesawCrimber : public LineTracer {
 private:
-	int16_t startTime;
-	int16_t counter;
-	int16_t speed;
+	int16_t s_time;
+	int16_t s_counter;
+	int16_t s_speed;
+	int16_t s_mode;
+	int16_t s_angle;
+	int16_t s_trace_counter = 0;
 
 protected:
 public:
